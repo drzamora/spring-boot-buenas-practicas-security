797f18b90c29762839a672f0d7ad494e2c9e28ec
Richard Zamora <77524442+drzamora@users.noreply.github.com>
2023-09-18 00:28:23 -0600

    Add files via upload

---
+++ api/src/main/java/med/voll/api/domain/medico/DatosRespuestaMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.domain.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.direccion.DatosDireccion;
+
+public record DatosRespuestaMedico(@NotNull Long id, String nombre, String email, String telefono, String documento, DatosDireccion direccion) {
+
+}
---
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -0,0 +1,49 @@
+package med.voll.api.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Le indicamos a Spring el tipo de sesion
+                .and().authorizeRequests()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                //Los usuarios autenticados y con perfil de ADMIN solo  pueden eliminar médicos y pacientes
+                //.requestMatchers(HttpMethod.DELETE, "/medicos").hasRole("ADMIN")
+                //.requestMatchers(HttpMethod.DELETE, "/pacientes").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager autenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
